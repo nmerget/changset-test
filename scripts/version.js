1011ae29bcc64e2768d112fea4d9b01aca51725c
Nicolas Merget <nicolas.merget@deutschebahn.com>
2025-05-31 00:35:07 +0200

    chore: custom version

---
+++ scripts/version.js
@@ -0,0 +1,11 @@
+import { writeFileSync } from "fs";
+import { execSync } from "child_process";
+
+function updateVersion() {
+  const version = `v${JSON.parse(execSync("pnpm pkg get version").toString().trim())}`;
+  const [major, minor, patch] = version.split(".");
+  const newContent = `MAJOR=${major}\nMINOR=${minor}\nPATCH=${patch}`;
+  writeFileSync("test.txt", newContent, "utf8");
+}
+
+updateVersion();
